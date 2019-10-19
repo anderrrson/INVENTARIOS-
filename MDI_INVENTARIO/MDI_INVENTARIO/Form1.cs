85fc678c3ec188c4606cf9d042dd5f0b2b3c383a
anderrrson <asandovalx@miumg.edu.gt>
2019-10-19 16:53:23 -0600

    MDI ACTUALIZADO

---
+++ MDI_INVENTARIO/MDI_INVENTARIO/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDI_INVENTARIO
+{
+    public partial class MDI_INVETARIO : Form
+    {
+        private int childFormNumber = 0;
+        string sIdUsuario="admin";
+        public MDI_INVETARIO()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowNewForm(object sender, EventArgs e)
+        {
+            Form childForm = new Form();
+            childForm.MdiParent = this;
+            childForm.Text = "Ventana " + childFormNumber++;
+            childForm.Show();
+        }
+        private void productosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_productosInventario nuevo = new frm_productosInventario(sIdUsuario);
+            //nuevo.MdiParent = this;
+            nuevo.Show();
+
+        }
+
+        private void tipoProductosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frm_tipoproducto tipo = new frm_tipoproducto(sIdUsuario);
+            tipo.Show();
+
+        }
+    }
+}
